f9ae08ac577dd216235094d1af89f4ef6b472681
Kris Jenkins <krisajenkins@gmail.com>
2025-12-19 11:57:36 +0000

    Update SpacetimeDB to v1.11.0

--- data.nix
+++ data.nix
@@ -1,27 +1,27 @@
 {
-  version = "1.10.0";
+  version = "1.11.0";
 
   # Latest version: curl -s https://api.github.com/repos/clockworklabs/SpacetimeDB/releases/latest | grep '"tag_name"'
 
   spacetimedbPkgs = {
     "aarch64-darwin" = [{
-      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.10.0/spacetime-aarch64-apple-darwin.tar.gz";
-      sha256 = "1p7rkdk063zarcjizpzmrljy5dr9h3sd184siwnka5hm3gh4hr8z";
+      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-aarch64-apple-darwin.tar.gz";
+      sha256 = "01aywdcp0xcbqm1b845hvpjwls83fp76i0865ynbb6mz4gzi3x1d";
     }];
 
     "x86_64-darwin" = [{
-      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.10.0/spacetime-x86_64-apple-darwin.tar.gz";
-      sha256 = "0djh3hps847nfj1lmxh152k03iykk0d9qbx0bw3zs5mdrxfzyln0";
+      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-x86_64-apple-darwin.tar.gz";
+      sha256 = "0l5hmxxw21g5r9fw6g33cvrax4n30iaqcji6440aiikgxgpjynn3";
     }];
 
     "x86_64-linux" = [{
-      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.10.0/spacetime-x86_64-unknown-linux-gnu.tar.gz";
-      sha256 = "19gnz3wfc1d97wl5aqwhk8nqf3hwlbyvmn88a39afq7kjgg8dgfz";
+      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-x86_64-unknown-linux-gnu.tar.gz";
+      sha256 = "1nyyyfhka7rlj4if5cz0f7dx0ls2nfcbdskja6cr47g442175ki8";
     }];
 
     "aarch64-linux" = [{
-      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.10.0/spacetime-aarch64-unknown-linux-gnu.tar.gz";
-      sha256 = "1l2igdk8k5zl4wmz87zwqgifffwl0bvd8fsymh8rscgiqcqdjphv";
+      url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-aarch64-unknown-linux-gnu.tar.gz";
+      sha256 = "00k25nd2c0l7nbksqyyrq3vb6faa24ias8vfx4cak3yk3vgfdn5m";
     }];
   };
 }
