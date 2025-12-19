f6077532cfbd023211c45da1722d40fd63a844aa
Kris Jenkins <krisajenkins@gmail.com>
2025-12-19 17:43:03 +0000

    Fix SHA256 hashes for v1.11.0
    Use hashes for the compressed tarball, not the unpacked contents.
    fetchurl hashes before unpacking.

--- data.nix
+++ data.nix
@@ -6,22 +6,22 @@
   spacetimedbPkgs = {
     "aarch64-darwin" = [{
       url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-aarch64-apple-darwin.tar.gz";
-      sha256 = "01aywdcp0xcbqm1b845hvpjwls83fp76i0865ynbb6mz4gzi3x1d";
+      sha256 = "00y6l6f71rj8ycsr5kqdn9h4mzjgcmxbmaxb93x4yga9sryczs5v";
     }];
 
     "x86_64-darwin" = [{
       url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-x86_64-apple-darwin.tar.gz";
-      sha256 = "0l5hmxxw21g5r9fw6g33cvrax4n30iaqcji6440aiikgxgpjynn3";
+      sha256 = "10y1pdxdr7s3h3vb9ackkk4xibp66agn2l8fv4g126qjh3j6v00i";
     }];
 
     "x86_64-linux" = [{
       url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-x86_64-unknown-linux-gnu.tar.gz";
-      sha256 = "1nyyyfhka7rlj4if5cz0f7dx0ls2nfcbdskja6cr47g442175ki8";
+      sha256 = "0m24kh8nlh9i05bbdg3h4v79vgj33gpv23jzdlqi45ad6yv4l1k3";
     }];
 
     "aarch64-linux" = [{
       url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.11.0/spacetime-aarch64-unknown-linux-gnu.tar.gz";
-      sha256 = "00k25nd2c0l7nbksqyyrq3vb6faa24ias8vfx4cak3yk3vgfdn5m";
+      sha256 = "1kgc41fsx4irijiwn5qjp5pc3zvkh9x0wb76x116lk77cyqiiihq";
     }];
   };
 }
